3461c697f496733b7cec6bbac3bd14d6b0806df9
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2023-02-23 18:10:51 +0500

    add view and api debugbar

---
+++ app/Http/Middleware/Debugbar.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Debugbar
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if ($response instanceof JsonResponse &&
+            app()->bound('debugbar') &&
+            app('debugbar')->isEnabled() &&
+            is_object($response->getData())
+        ) {
+            $response->setData($response->getData(true) + [
+                '_debugbar' => app('debugbar')->getData()
+            ]);
+        }
+
+        return $response;
+    }
+}
